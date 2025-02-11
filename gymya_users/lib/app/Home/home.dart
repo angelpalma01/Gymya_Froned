8546a03ed895ded9fba5c6ebf5e15c9247e9fd94
levos04 <leviolevos@gmail.com>
2025-02-10 23:40:26 -0600

    Genera código QR pero tapa botones

--- gymya_users/lib/app/Home/home.dart
+++ gymya_users/lib/app/Home/home.dart
@@ -3,7 +3,8 @@ import 'package:table_calendar/table_calendar.dart';
 import 'package:gymya_users/app/historial/historial_entradas.dart';
 import 'package:intl/intl.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';  // Para decodificar JSON
+import 'package:barcode_widget/barcode_widget.dart'; // Para generar el código QR
+import 'dart:convert';
 
 class DashboardScreen extends StatefulWidget {
   final String token;
@@ -20,7 +21,56 @@ class _DashboardScreenState extends State<DashboardScreen> {
   CalendarFormat _calendarFormat = CalendarFormat.week;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
-  Future<String>? _qrFuture; // Variable para manejar la llamada de la API que genera el QR
+
+  DateTime? _expiryDate;
+  String? _membresiaId;
+  String? _planId;
+  bool isLoading = true;
+
+  // Endpoint de membresía
+  final String membresiaUrl = 'https://api-gymya-api.onrender.com/api/membresia';
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchMembresiaData();
+  }
+
+  Future<void> _fetchMembresiaData() async {
+    try {
+      final response = await http.get(
+        Uri.parse(membresiaUrl),
+        headers: {
+          'Authorization': 'Bearer ${widget.token}', // Pasa el token en la cabecera
+        },
+      );
+
+      if (response.statusCode == 200) {
+        // Parsear los datos de la respuesta
+        final List<dynamic> data = jsonDecode(response.body); // Parseamos la respuesta como lista
+
+        if (data.isNotEmpty) {
+        final membresia = data[0]; // Accedemos al primer elemento de la lista
+
+        setState(() {
+          _expiryDate = DateTime.parse(membresia['fecha_fin']); // Fecha de fin de la membresía
+          _membresiaId = membresia['membresia_id'].toString(); // ID de la membresía
+          _planId = membresia['plan_id'].toString(); // ID del plan
+          isLoading = false;
+        });
+        } else {
+          throw Exception('No se encontraron datos de membresía');
+        }
+      } else {
+        throw Exception('Error al obtener la membresía');
+      }
+    } catch (error) {
+      print('Error: $error');
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
 
   void _onItemTapped(int index) {
     setState(() {
@@ -28,89 +78,87 @@ class _DashboardScreenState extends State<DashboardScreen> {
     });
   }
 
-  // Método para generar el QR mediante una petición HTTP
-  Future<String> generarQR() async {
-    try {
-      final response = await http.get(
-        Uri.parse('https://api-gymya-api.onrender.com/api/user/qr'), // Cambia la URL por la de tu API
-        headers: {
-          'Authorization': 'Bearer ${widget.token}',  // Enviar el token en la cabecera
-        },
-      );
-
-      if (response.statusCode == 201) {
-        final jsonResponse = jsonDecode(response.body);
-        return jsonResponse['qrCode'];  // Aquí se devuelve el valor en Base64 del QR
-      } else {
-        throw Exception('Error al generar el código QR');
-      }
-    } catch (error) {
-      throw Exception('Error al conectar con la API: $error');
-    }
-  }
-
-  // Método para iniciar la generación del QR
-  void _onEnterPressed() {
-    setState(() {
-      _qrFuture = generarQR();  // Asignar la llamada a la API cuando se presiona el botón
-    });
-  }
-
-  @override
+   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              _buildHeader(),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
+        child: isLoading
+            ? Center(child: CircularProgressIndicator()) // Muestra un cargador mientras se obtienen los datos
+            : SingleChildScrollView(
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const SectionTitle(title: 'Estado de membresía:'),
-                    const Text(
-                      'Mantén tu membresía activa para seguir disfrutando de los beneficios.',
-                      style: TextStyle(color: Colors.grey, fontSize: 14),
-                    ),
-                    const SizedBox(height: 16),
-                    MembershipCard(expiryDate: '25 noviembre 2024'),
-                    const SizedBox(height: 16),
-                    const SectionTitle(title: 'Entrada al gimnasio:'),
-                    const Text(
-                      'Escanea el código QR para acceder al gimnasio y observa tu historial de visitas.',
-                      style: TextStyle(color: Colors.grey, fontSize: 14),
-                    ),
-                    const SizedBox(height: 16),
-                    VisitCard(
-                      lastVisitDate: '23 noviembre 2024',
-                      onEnterPressed: _onEnterPressed,
-                      onHistoryPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => HistorialEntradasScreen(
-                              token: widget.token,
-                              user: widget.user,
-                            ),
+                    _buildHeader(),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SectionTitle(title: 'Estado de membresía:'),
+                          const Text(
+                            'Mantén tu membresía activa para seguir disfrutando de los beneficios.',
+                            style: TextStyle(color: Colors.grey, fontSize: 14),
                           ),
-                        );
-                      },
+                          const SizedBox(height: 16),
+                          _buildMembershipCard(), // Fecha fin de la membresía
+                          const SizedBox(height: 16),
+                          const SectionTitle(title: 'Entrada al gimnasio:'),
+                          const Text(
+                            'Escanea el código QR para acceder al gimnasio.',
+                            style: TextStyle(color: Colors.grey, fontSize: 14),
+                          ),
+                          const SizedBox(height: 16),
+                          _buildQRCode(), // Generar código QR
+                          const SizedBox(height: 16),
+                          const SectionTitle(title: 'Calendario:'),
+                          _buildCalendar(),
+                        ],
+                      ),
                     ),
-                    const SizedBox(height: 16),
-                    const SectionTitle(title: 'Calendario:'),
-                    _buildCalendar(),
                   ],
                 ),
               ),
-            ],
-          ),
-        ),
       ),
       bottomNavigationBar: _buildBottomNavigationBar(),
     );
+  }
+
+  Widget _buildQRCode() {
+    // Verifica si ya tenemos los datos de la membresía y genera el QR
+    if (_membresiaId != null && _planId != null && _expiryDate != null) {
+      final qrData = {
+        'membresia_id': _membresiaId,
+        'plan_id': _planId,
+        'fecha_fin': _expiryDate!.toIso8601String(),
+      };
+
+      return Center(
+        child: BarcodeWidget(
+          barcode: Barcode.qrCode(), // Convertimos los datos del QR a un JSON
+          data: jsonEncode(qrData),
+          width: 200,
+          height: 200,
+          backgroundColor: Colors.white,  // Fondo blanco para el QR
+          color: Colors.black,  // Color del código QR
+        ),
+      );
+    } else {
+      return const Center(
+        child: Text(
+          'Cargando datos de membresía...',
+          style: TextStyle(color: Colors.white),
+        ),
+      );
+    }
+  }
+
+  Widget _buildMembershipCard() {
+  // Verificar si la fecha de expiración no es null
+  String expiryDateString = _expiryDate != null
+      ? DateFormat('dd MMMM yyyy').format(_expiryDate!) // Formatear fecha
+      : 'No disponible';
+
+  return MembershipCard(expiryDate: expiryDateString);
   }
 
   Widget _buildHeader() {
@@ -139,7 +187,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
           ),
           const CircleAvatar(
             radius: 24,
-            backgroundImage: AssetImage('assets/profile_picture.png'),
+            //backgroundImage: AssetImage('assets/profile_picture.png'),
           ),
         ],
       ),
